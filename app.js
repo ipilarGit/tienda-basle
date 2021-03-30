28d51aeb618eb8a220355dc29a103bdd4d934e3d
ipilarGit <70710932+ipilarGit@users.noreply.github.com>
2021-03-30 00:45:08 -0300

    prueba de conexion

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Middlewares & Config
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3050;
+app.listen(PORT, () => {
+    console.log('Servidor Encendido')
+});
+
+const { getProducts } = './consultas';
+
+// Route
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+});
+
+// Todos los productos
+app.get('/products', (req, res) => {
+    try {
+        const productos = await getProducts();
+        res.send(productos);
+    } catch {
+        console.log(e);
+        res.status(500).send(
+            {
+                error: "500 Internal Server Error",
+                message: e
+            });
+    }
+})
+
+
+
---
+++ consultas.js
@@ -0,0 +1,23 @@
+// MySql
+const connection = mysql.createConnection({
+    user: "bsale_test",
+    host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+    password: "bsale_test",
+    database: "bsale_test",
+  });
+  
+// Check connect
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Database server running!');
+  });
+
+const getProducts = async()=>{
+    const sql = await connection.query('SELECT * FROM bsale_test.product');
+    return sql.rows;
+
+}
+
+module.exports = {
+    getProducts
+}
