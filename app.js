2709c75a976ff6a42cfb376fc4868f7804f57694
ipilarGit <70710932+ipilarGit@users.noreply.github.com>
2021-03-31 03:01:47 -0300

    catalogo por categoria

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
 const express = require('express');
-const app = express();
-const mysql = require('mysql');
+const exphbs = require("express-handlebars");
+const Handlebars = require("handlebars");
+const path = require("path");
 const bodyParser = require("body-parser");
 
+const app = express();
+
+const connection = require('./conection');
+
 // Settings
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middlewares & Config
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main",
+        layoutsDir: path.join(__dirname, "./views/mainLayout"),
+        partialsDir: path.join(__dirname, "./views/components"),
+    })
+);
+app.set("view engine", "handlebars");
+
+Handlebars.registerHelper("prev", (pag) => (pag == 1 ? 1 : Number(pag) - 1));
+
+Handlebars.registerHelper("next", (pag, max) =>
+    pag == max ? pag : Number(pag) + 1
+);
 
 // Start server
 app.listen(PORT, () => {
     console.log('Servidor Encendido')
 });
 
-const { getProducts } = './consultas';
 
 // Routes
+/* app.get('/', (req, res) => {
+    res.send('Bienvenido');
+}); */
+
+// all products por category
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
+
+    const sql = 'SELECT * FROM product ORDER BY category';
+
+    connection.query(sql, (error, productos) => {
+        if (error) { res.json(error) };
+        
+        if (productos.length >= 0) {
+            console.log({ data: productos });
+            //   let productos = rows;
+            const { pag } = req.query;
+            const pageQ = Math.ceil(productos.length / 10);
+            pag
+                ? (productos = productos.slice(pag * 10 - 10, pag * 10))
+                : (productos = productos.slice(0, 10));
+            res.render("Catalogo", {
+                //  productos: JSON.stringify(products),
+                productos,
+                pageQ,
+                pag: pag || 1,
+            });
+            //  res.json(results);
+        } else {
+            res.send('Not result');
+        }
+    });
+
 });
 
-// Todos los productos
-app.get('/products', async(req, res) => {
-    try {
-        const productos = await getProducts();
-
-        res.send(productos);
-    } catch {
-        console.log(e);
-        res.status(500).send(
-            {
-                error: "500 Internal Server Error",
-                message: e
-            });
-    }
-})
+app.get("*", (req, res) => {
+    res.render("Error");
+  });
 
 
 
---
+++ conection.js
@@ -0,0 +1,18 @@
+// MySql
+const mysql = require('mysql');
+const config = {
+    host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
+    user: "bsale_test",
+    password: "bsale_test",
+    database: "bsale_test"
+}
+
+const connection = mysql.createConnection(config);
+  
+// Check connect
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Database server running!');
+  }); 
+
+module.exports =  connection;
