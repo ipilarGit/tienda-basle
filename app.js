3eb5bf070e90e0ce38959d735e86ff172c153a31
ipilarGit <70710932+ipilarGit@users.noreply.github.com>
2021-04-02 06:33:16 -0300

    busqueda de productos

--- app.js
+++ app.js
@@ -34,14 +34,11 @@ Handlebars.registerHelper("next", (pag, max) =>
 
 // Start server
 app.listen(PORT, () => {
-    console.log('Servidor Encendido')
+    console.log('Server ON')
 });
 
 
 // Routes
-/* app.get('/', (req, res) => {
-    res.send('Bienvenido');
-}); */
 
 // all products por category
 app.get('/', (req, res) => {
@@ -50,10 +47,8 @@ app.get('/', (req, res) => {
 
     connection.query(sql, (error, productos) => {
         if (error) { res.json(error) };
-        
         if (productos.length >= 0) {
-            console.log({ data: productos });
-            //   let productos = rows;
+           // console.log({ data: productos });
             const { pag } = req.query;
             const pageQ = Math.ceil(productos.length / 10);
             pag
@@ -65,17 +60,55 @@ app.get('/', (req, res) => {
                 pageQ,
                 pag: pag || 1,
             });
-            //  res.json(results);
         } else {
             res.send('Not result');
         }
     });
-
 });
 
-app.get("*", (req, res) => {
+app.get("/busqueda/:filtro/:input", (req, res) =>{
+
+    const sql = 'SELECT * FROM product';
+
+    connection.query(sql, (error, productos) => {
+        if (error) { res.json(error) };
+console.log(productos);
+    let filtros = Object.keys(productos[0]);
+
+    console.log('filtros:  ',filtros);
+//    let filtros = productos;
+    const { filtro, input } = req.params;
+
+   let all_ProductsLabels = productos.map((p) => p.category);
+
+    all_ProductsLabels = [...new Set(all_ProductsLabels)];
+    if (filtro == "todos") {
+        productos = productos.filter((p) => {
+          let valoresDeBusqueda = Object.values(p).map((v) =>{
+              if ( (v)!=null){
+            v.toString().toLocaleLowerCase()}}
+          );
+          return valoresDeBusqueda.includes(input.toLocaleLowerCase());
+        });
+      } else {
+        productos = productos.filter((p) => {
+          return p[filtro].toString().toLowerCase().includes(input.toLowerCase());
+        });
+      }
+  
+    res.render("Busqueda", {
+      productos: productos.length >= 1 ? productos : null,
+      all_ProductsLabels,
+      filtros,
+    });
+
+    })
+})
+
+
+/* app.get("*", (req, res) => {
     res.render("Error");
   });
 
-
+ */
 
