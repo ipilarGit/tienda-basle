2b54a3ea7f49f2a0201007f7d05d2b5cf138ea2e
ipilarGit <70710932+ipilarGit@users.noreply.github.com>
2021-04-02 22:54:03 -0300

    agregar al carrito

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const connection = require('./conection');
-
+const { catalogo, productSearch , getProductById} = require("./controllers/main");
 // Settings
 const PORT = process.env.PORT || 3001;
 
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/assets", express.static(__dirname + "/assets"));
-
 app.engine(
     "handlebars",
     exphbs({
@@ -37,78 +36,18 @@ app.listen(PORT, () => {
     console.log('Server ON')
 });
 
-
 // Routes
 
 // all products por category
-app.get('/', (req, res) => {
+app.get('/', catalogo);
 
-    const sql = 'SELECT * FROM product ORDER BY category';
+app.get("/busqueda/:filtro/:input", productSearch)
 
-    connection.query(sql, (error, productos) => {
-        if (error) { res.json(error) };
-        if (productos.length >= 0) {
-           // console.log({ data: productos });
-            const { pag } = req.query;
-            const pageQ = Math.ceil(productos.length / 10);
-            pag
-                ? (productos = productos.slice(pag * 10 - 10, pag * 10))
-                : (productos = productos.slice(0, 10));
-            res.render("Catalogo", {
-                //  productos: JSON.stringify(products),
-                productos,
-                pageQ,
-                pag: pag || 1,
-            });
-        } else {
-            res.send('Not result');
-        }
-    });
-});
+app.get("/producto/:id", getProductById);
 
-app.get("/busqueda/:filtro/:input", (req, res) =>{
-
-    const sql = 'SELECT * FROM product';
-
-    connection.query(sql, (error, productos) => {
-        if (error) { res.json(error) };
-console.log(productos);
-    let filtros = Object.keys(productos[0]);
-
-    console.log('filtros:  ',filtros);
-//    let filtros = productos;
-    const { filtro, input } = req.params;
-
-   let all_ProductsLabels = productos.map((p) => p.category);
-
-    all_ProductsLabels = [...new Set(all_ProductsLabels)];
-    if (filtro == "todos") {
-        productos = productos.filter((p) => {
-          let valoresDeBusqueda = Object.values(p).map((v) =>{
-              if ( (v)!=null){
-            v.toString().toLocaleLowerCase()}}
-          );
-          return valoresDeBusqueda.includes(input.toLocaleLowerCase());
-        });
-      } else {
-        productos = productos.filter((p) => {
-          return p[filtro].toString().toLowerCase().includes(input.toLowerCase());
-        });
-      }
-  
-    res.render("Busqueda", {
-      productos: productos.length >= 1 ? productos : null,
-      all_ProductsLabels,
-      filtros,
-    });
-
-    })
-})
-
-
-/* app.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.render("Error");
   });
 
- */
+
 
---
+++ controllers/main.js
@@ -0,0 +1,105 @@
+const connection = require("../conection.js");
+
+const catalogo = (req, res) => {
+
+    const sql = 'SELECT * FROM product ORDER BY category';
+    connection.query(sql, (error, productos) => {
+        if (error) { res.json(error) };
+        if (productos) {
+            const { pag } = req.query;
+            const pageQ = Math.ceil(productos.length / 10);
+            pag
+                ? (productos = productos.slice(pag * 10 - 10, pag * 10))
+                : (productos = productos.slice(0, 10));
+            res.render("Catalogo", {
+                productos,
+                pageQ,
+                pag: pag || 1,
+            });
+        } else {
+            res.send('Not result');
+        }
+    });
+}
+
+const productSearch = (req, res) => {
+
+    const sql = 'SELECT * FROM product';
+
+    connection.query(sql, (error, productos) => {
+        if (error) { res.json(error) };
+        console.log(productos);
+        let filtrosss = Object.keys(productos[0]);
+
+        filtros = filtrosss.filter((f) => f != 'url_image');
+
+        console.log('filtros:  ', filtros);
+        //    let filtros = productos;
+        const { filtro, input } = req.params;
+
+        let all_ProductsLabels = productos.map((p) => p.category);
+
+        all_ProductsLabels = [...new Set(all_ProductsLabels)];
+        if (filtro == "todos") {
+            productos = productos.filter((p) => {
+                let valoresDeBusqueda = Object.values(p).map((v) => {
+                    if ((v) != null) {
+                        v.toString().toLocaleLowerCase()
+                    }
+                }
+                );
+                return valoresDeBusqueda.includes(input.toLocaleLowerCase());
+            });
+        } else {
+            productos = productos.filter((p) => {
+                return p[filtro].toString().toLowerCase().includes(input.toLowerCase());
+            });
+        }
+
+        res.render("Busqueda", {
+            productos: productos.length >= 1 ? productos : null,
+            all_ProductsLabels,
+            filtros,
+        });
+    })
+}
+
+const getProductById = (req, res) => {
+
+    const { id } = req.params;
+    console.log(id);
+    
+    const sql = 'SELECT * FROM product';
+    connection.query(sql, (error, products) => {
+        if (error) { res.json(error) };
+     let productos = products;
+
+     const sql = 'SELECT * FROM category';
+     connection.query(sql, (error, categoriess) => {
+         if (error) { res.json(error) };
+      let categories= categoriess;
+      productos.forEach((p) => {
+        p.category = categories.find((c) => c.id == p.category).name;
+      });
+      let producto = productos.find((p) => p.id == id);
+      res.render("Details", { producto, productoData: JSON.stringify(producto) });
+    
+ 
+
+     })  
+    
+    })  
+ 
+
+/* 
+    const promiseOptions = await Promise.all([getProducts(), getCategories()]);
+  const [productos, categories] = promiseOptions; */
+ /*  productos.forEach((p) => {
+    p.category = categories.find((c) => c.id == p.category).name;
+  });
+  let producto = productos.find((p) => p.id == id);
+  res.render("Details", { producto, productoData: JSON.stringify(producto) });
+*/
+
+} 
+module.exports = { catalogo, productSearch, getProductById }
